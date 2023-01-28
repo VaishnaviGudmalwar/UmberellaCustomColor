49a6cdf6abf9d2536f645ac4788f7a3622742502
VaishnaviGudmalwar <113743593+VaishnaviGudmalwar@users.noreply.github.com>
2023-01-28 13:10:12 +0530

    first commit

---
+++ ProjectCustom.js
@@ -0,0 +1,41 @@
+// var allUmbrellaImg=["./Pinkumbrella.png","./Blueumbrella.png","./Yelloumbrella.png","./Pinkumbrella.png","./Pinkumbrella.png","./Pinkumbrella.png","./Pinkumbrella.png","./Pinkumbrella.png"];
+// var imgPro = document.querySelector('#imgBox');
+// var allcolors=document.querySelectorAll('.Main-Product-child-2-details-color-bullets-circles');
+// console.log(allcolors[0].classList);
+// allcolors.forEach(colors=>colors.addEventListener('click',function (elem){
+//   for ( var i = 0; i < allcolors.length;  i++) {
+//     allcolors[i].classList.contains
+//     if(allcolors.classList.contains('circle1')){
+//         imgPro.src="./Pinkumbrella";
+//         document.body.style.backgroundColor="#ffe6e6";
+//     }
+//     if(allcolors.classList.contains('circle2')){
+//         imgPro.src='./' +allUmbrellaImg[1]+'.png';
+//         document.body.style.backgroundColor="#f2f2f2"
+//     }
+//     if(allcolors.classList.contains('circle3')){
+//         imgPro.src='./' +allUmbrellaImg[2]+'.png';
+//         document.body.style.backgroundColor="#f2f2f2";
+//     }
+//     if(allcolors.classList.contains('circle4')){
+//         imgPro.src='./' +allUmbrellaImg[3]+'.png';
+//         document.body.style.backgroundColor="#ffe6f0";
+//     }
+//     if(allcolors.classList.contains('circle5')){
+//         imgPro.src='./' +allUmbrellaImg[4]+'.png';
+//         document.body.style.backgroundColor="#b3e6ff";
+//     }
+//     if(allcolors.classList.contains('circle6')){
+//         imgPro.src='./ '+allUmbrellaImg[5]+'.png';
+//         document.body.style.backgroundColor="#ffff99";
+//     }
+//     if(allcolors.classList.contains('circle7')){
+//         imgPro.src='./' +allUmbrellaImg[6]+'.png';
+//         document.body.style.backgroundColor="#ffebcc";
+//     }
+//     if(allcolors.classList.contains('circle8')){
+//         imgPro.src='./' +allUmbrellaImg[7]+'.png';
+//         document.body.style.backgroundColor="#ffcccc";
+//     }
+//   }
+// }))
